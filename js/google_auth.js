50d62cc9ee0da3c5e1a5be5b1450b4cccc85b7fc
Charlie Shehadi <cshehadi@gmail.com>
2016-08-17 15:27:26 -0400

    use auth2, v3, resolve permission-related 404s

--- js/google_auth.js
+++ js/google_auth.js
@@ -1,30 +1,36 @@
 
       // The Browser API key obtained from the Google Developers Console.
-      var developerKey = 'AIzaSyBrmIh_QbGVjEojoBmkRTh7Ue0vH70izXk';
+      var apiKey = 'AIzaSyBrmIh_QbGVjEojoBmkRTh7Ue0vH70izXk';
 
       // The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
       var clientId = "182401266382-366sj3a9m2nf7kgn0dpq977qurkljodd.apps.googleusercontent.com"
 
       // Scope to use to access user's photos.
-      var scope = ['https://www.googleapis.com/auth/drive.readonly'];
+      var scopes = ['https://www.googleapis.com/auth/drive.readonly'];
+
+      var auth2;
 
       var pickerApiLoaded = false;
       var oauthToken;
 
       // Use the API Loader script to load google.picker and gapi.auth.
       function onApiLoad() {
-        gapi.load('auth', {'callback': onAuthApiLoad});
-        gapi.load('picker', {'callback': onPickerApiLoad});
+        gapi.load('client:auth2:picker', {'callback': initAuth});
       }
 
-      function onAuthApiLoad() {
-        window.gapi.auth.authorize(
-            {
-              'client_id': clientId,
-              'scope': scope,
-              'immediate': false
-            },
-            handleAuthResult);
+      function initAuth() {
+          gapi.client.setApiKey(apiKey);
+          gapi.auth2.init({
+              client_id: clientId,
+              scope: scopes.join(' '),
+              immediate: false,
+          }).then(function () {
+            auth2 = gapi.auth2.getAuthInstance();
+            auth2.signIn().then(function () {
+                oauthToken = auth2.currentUser.get().hg.access_token;
+                onPickerApiLoad();
+            });
+          });
       }
 
       function onPickerApiLoad() {
@@ -45,8 +51,6 @@
           var picker = new google.picker.PickerBuilder().
               addView(google.picker.ViewId.DOCUMENTS).
               setOAuthToken(oauthToken).
-              setDeveloperKey(developerKey).
-              setOrigin(window.location.protocol + '//' + window.location.host).
               setCallback(pickerCallback).
               build();
           picker.setVisible(true);
@@ -59,17 +63,30 @@
 
     function pickerCallback(data) {
       if (data.action == google.picker.Action.PICKED) {
+
         var fileId = data.docs[0].id;
         var mimeType = data.docs[0].mimeType;
         var export_data;
 
         if (mimeType == 'application/vnd.google-apps.document') {
-            export_data = {mimeType: 'text/html', key: developerKey};
-            $.get('https://www.googleapis.com/drive/v3/files/'+fileId+'/export', export_data, handleImport);
+            export_data = {mimeType: 'text/html', key: apiKey};
+            $.ajax({
+                    url: 'https://www.googleapis.com/drive/v3/files/'+fileId+'/export', 
+                    data: export_data, 
+                    beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer '+oauthToken); },
+                    success: handleImport 
+                });
         } else {
-            export_data = {key: developerKey};
-            $.get('https://www.googleapis.com/drive/v3/files/0B5qjOodUtEthZENxS1VUQlByX3c')
-            $.get('https://www.googleapis.com/drive/v3/files/'+fileId, export_data, function(data) { console.log(data); });
+            $('#result').html('<p><strong>Sorry</strong> - at this time we can only import documents that were <em>created</em> in Google Docs.  This document seems to have been created in another application and uploaded to Google Docs.</p>');
+/*          // use this code to download a non-google doc.  You'll need some method to convert it to html
+            gapi.client.load('drive', 'v3', function() {
+                var request = gapi.client.drive.files.get({
+                  fileId: fileId,
+                  alt: 'media'
+                }).then(function(file) {
+                    // send to submission server?
+                });
+            }); */
         }
       }
     }
